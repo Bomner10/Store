6212f1c810b88e59c667016d72089984d2d1257c
Bomner10 <cmelectronicice@gmail.com>
2024-08-25 02:15:06 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,39 @@
-const CACHE_NAME = 'sistema-venta-cache-v1';
+const CACHE_NAME = 'venta-simple-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/styles.css',
     '/script.js',
     '/manifest.json',
-    '/service-worker.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/icon-192x192.png',
+    '/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+// Instalación del Service Worker
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
+            .then(cache => {
+                console.log('Archivos en caché');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+// Activación del Service Worker
+self.addEventListener('activate', event => {
+    console.log('Service Worker activado');
+});
+
+// Interceptar solicitudes de red
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
+            .then(response => {
+                if (response) {
+                    return response; // Devuelve la respuesta desde la caché
+                }
+                return fetch(event.request); // Realiza la solicitud a la red
             })
     );
 });
